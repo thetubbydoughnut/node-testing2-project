f8d1ab199a3f752a47eef89c1635b680de8482b2
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-11-02 17:54:18 -0500

    wip

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const db = require('../data/dbConfig')
 const server = require('../server')
+const Joke = require('./jokesModel')
 
 const joke1 = {joke:'Why did the chicken cross the road?', punchline:"Because it was free range"}
 const joke2 = {joke:'Why did the chicken cross the road?', punchline:"To avoid this lame and outdated joke"}
@@ -18,8 +19,35 @@ afterAll(async () => {
     await db.destroy()
 })
 
-
-
 it('correct env var', () => {
     expect(process.env.DB_ENV).toBe('testing')
+})
+
+describe('Jokes model functions', () => {
+    describe('create joke', () => {
+        it('add joke to the db', async () => {
+            let jokes
+            await Joke.createJoke(joke1)
+            jokes = await db('jokes')
+            expect(jokes).toHaveLength(1)
+
+            await Joke.createJoke(joke2)
+            jokes = await db('jokes')
+            expect(jokes).toHaveLength(2)
+        })
+        it('inserted joke and punchline', async () => {
+            const joke = await Joke.createJoke(joke1)
+            expect(joke).toMatchObject({joke_id:1, ...joke})
+        })
+    })
+    describe("[DELETE] / - deletes joke", () => {
+        it('removes joke from db', async () => {
+            const [joke_id] = await db('jokes').insert(joke1)
+            let joke = await db('jokes').where({joke_id}).first()
+            expect(joke).toBeTruthy()
+            await request(server).delete('/jokes/'+ joke_id)
+            joke = await db('jokes').where({joke_id}).first()
+            expect(joke).toBeFalsy()
+        })
+    })
 })
